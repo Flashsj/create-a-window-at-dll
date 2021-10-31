eb75aafb4372f946fe3018a6967ceb5e1ff5a3f2
Flashsj <41318875+Flashsj@users.noreply.github.com>
2021-10-31 10:54:12 +0800

    Add files via upload

---
+++ MFCLibrary1/CMyDialog.cpp
@@ -0,0 +1,34 @@
+﻿// CMyDialog.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "MFCLibrary1.h"
+#include "CMyDialog.h"
+#include "afxdialogex.h"
+
+
+// CMyDialog 对话框
+
+IMPLEMENT_DYNAMIC(CMyDialog, CDialog)
+
+CMyDialog::CMyDialog(CWnd* pParent /*=nullptr*/)
+	: CDialog(IDD_DIALOG1, pParent)
+{
+
+}
+
+CMyDialog::~CMyDialog()
+{
+}
+
+void CMyDialog::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CMyDialog, CDialog)
+END_MESSAGE_MAP()
+
+
+// CMyDialog 消息处理程序
